35ce92fdc5bc11f8045f8fe5fcf8b9748ee84276
Gabriel Barbosa <gabriel@versallini.com.br>
2018-01-20 15:00:38 -0200

    allow filtering by exchanges and currencies (pairs)

--- core/market.js
+++ core/market.js
@@ -1,89 +1,97 @@
 'use strict';
 
-const ccxt = require ('ccxt');
+const ccxt = require('ccxt');
+const configs = require('../config/settings');
 const arbitrage = require('./arbitrage');
 
 exports.initialize = async function() {
-    try {
-        
-        let tickets = await prepareTickets();
-        
-        for (let ticket of tickets) {
-            try {
-                startArbitrageByTicket(ticket);
-            } catch (error) {
-                console.error('Error:', error.message);
-            }
-        }
-    
-    } catch (error) {
+  try {
+    const tickets = await prepareTickets();
+    for (let ticket of tickets) {
+      try {
+        startArbitrageByTicket(ticket);
+      } catch (error) {
         console.error('Error:', error.message);
+      }
     }
-}
-
-async function prepareTickets() {
-    let ids = [];
-    let exchanges = {}
-
-    for (let id of ccxt.exchanges) {
-        try {
-            
-            let exchange = new ccxt[id]();
-
-            exchanges[id] = exchange;
-    
-            let markets = await exchange.loadMarkets();
-            
-            ids.push(id);
-            
-        } catch (error) {
-            console.error('Error:', error.message);
-        }
-    }
-
-    let uniqueSymbols = ccxt.unique(ccxt.flatten(ids.map(id => exchanges[id].symbols)));
-
-    let arbitrableSymbols = uniqueSymbols.filter(symbol => ids.filter(id => 
-        (exchanges[id].symbols.indexOf(symbol) >= 0)).length > 1)
-            .sort((id1, id2) => (id1 > id2) ? 1 : ((id2 > id1) ? -1 : 0));
-        
-    let tickets = arbitrableSymbols.map (symbol => {
-        let row = { symbol, exchanges:[] };
-        for (let id of ids)
-            if (exchanges[id].symbols.indexOf (symbol) >= 0)
-                row.exchanges.push(id);
-        return row
-    });
-    
-    return tickets;
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
 }
 
 async function startArbitrageByTicket(ticket) {
-    console.log(ticket);
-    
+  setInterval(function() {
     try {
-        // Promise.all()
-        // .then((response) => {
-        //     console.log(response);
-        //     arbitrage.checkOpportunity(response);
-        // }).catch((error)=> {
-        //     console.error('Error:', error.message);
-        // });
-    } catch (error) {
+      let promises = ticket.exchanges.map(async (exchange) =>
+        Promise.resolve(await fetchDataByTicketAndExchange(
+          ticket.symbol, exchange)));
+
+      Promise.all(promises).then((response) => {
+        arbitrage.checkOpportunity(response);
+      }).catch((error) => {
         console.error('Error:', error.message);
-    }   
+      });
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
+  }, 30 * 1000);
 }
 
 async function fetchDataByTicketAndExchange(ticket, exchangeName) {
-  let exchange = new ccxt[exchangeName]();
+  const exchange = new ccxt[exchangeName]();
   const market = await exchange.fetchTicker(ticket);
-  
+
   return {
-      name: exchange.constructor.name,
-      ticket: ticket,
-      cost: 0.005,
-      bid: market.bid,
-      ask: market.ask
-  };
+    name: exchangeName,
+    ticket: ticket,
+    cost: 0.005,
+    bid: market.bid,
+    ask: market.ask
+  }
 }
 
+async function prepareTickets() {
+  let api = {}
+  let exchanges = [];
+
+  if (configs.filter_exchanges) {
+    exchanges = configs.exchanges;
+  } else {
+    exchanges = ccxt.exchanges;
+  }
+
+  for (let name of exchanges) {
+    try {
+      let exch = new ccxt[name]();
+      api[name] = exch;
+      let markets = await exch.loadMarkets();
+      if (!configs.filter_exchanges) {
+        exchanges.push(name);
+      }
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
+  }
+
+  let symbols = [];
+  ccxt.unique(ccxt.flatten(exchanges.map(name => api[name].symbols))).filter(symbol =>
+    ((configs.filter_tickets) ? configs.tickets.map(tn =>
+      (symbol.indexOf(tn) >= 0) ? symbols.push(symbol) : 0) : symbols.push(symbol)));
+
+  let arbitrables = symbols.filter(symbol => exchanges.filter(name =>
+    (api[name].symbols.indexOf(symbol) >= 0)).length > 1).sort((id1, id2) =>
+    (id1 > id2) ? 1 : ((id2 > id1) ? -1 : 0));
+
+  let tickets = arbitrables.map(symbol => {
+    let row = {
+      symbol,
+      exchanges: []
+    };
+    for (let name of exchanges)
+      if (api[name].symbols.indexOf(symbol) >= 0)
+        row.exchanges.push(name);
+    return row
+  });
+
+  return tickets;
+}
